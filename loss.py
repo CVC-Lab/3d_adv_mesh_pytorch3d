c1c6c3f2eda9631af624e7c110fa846e28d8da84
mkzhu-2000 <71708971+mkzhu-2000@users.noreply.github.com>
2020-10-24 07:50:35 +0800

    Update loss.py
    change TV loss to fit atlas

--- loss.py
+++ loss.py
@@ -78,9 +78,9 @@ class TotalVariation(nn.Module):
     
     def forward(self, adv_patch):
         # adv_patch : (H, W, C)
-        tvcomp1 = torch.sum(torch.abs(adv_patch[:, 1:, :] - adv_patch[:, :-1, :] + 0.000001), 0)
-        tvcomp1 = torch.sum(torch.sum(tvcomp1, 0), 0)
-        tvcomp2 = torch.sum(torch.abs(adv_patch[1:, :, :] - adv_patch[:-1, :, :] + 0.000001), 0)
-        tvcomp2 = torch.sum(torch.sum(tvcomp2, 0), 0)
+        tvcomp1 = torch.sum(torch.abs(adv_patch[:, 1:, :, :] - adv_patch[:, :-1, :, :] + 0.000001), 0)
+        tvcomp1 = torch.sum(torch.sum(torch.sum(tvcomp1, 0), 0), 0)
+        tvcomp2 = torch.sum(torch.abs(adv_patch[:, :, 1:, :] - adv_patch[:, :, :-1, :] + 0.000001), 0)
+        tvcomp2 = torch.sum(torch.sum(torch.sum(tvcomp2, 0), 0), 0)
         tv = tvcomp1 + tvcomp2
         return tv / torch.numel(adv_patch)
